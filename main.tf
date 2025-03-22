26ecd3b1e16eef3e8a72c673b12c18f7db944594
Made By Dom <hello@madebydom.dev>
2025-03-22 09:09:34 +0000

    add ts_state bucket with versioning

--- main.tf
+++ main.tf
@@ -11,3 +11,17 @@ provider "aws" {
   region  = "eu-north-1"
   profile = "sso"
 }
+
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "tf-state-${data.aws_region.current.name}-${data.aws_caller_identity.current.account_id}"
+}
+
+resource "aws_s3_bucket_versioning" "tf_state" {
+  bucket = aws_s3_bucket.tf_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
